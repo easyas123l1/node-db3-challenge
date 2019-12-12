6ca561690009c71a01fdc25e1a4c51b3a73242b8
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-11 19:55:51 -0600

    findSteps and add schemes

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -17,3 +17,14 @@ function findById(id) {
   return db('schemes').where({ id }).first();
 }
 
+function findSteps(id) {
+  return db('schemes').select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions').join('steps', 'schemes.id', 'steps.scheme_id').where({ scheme_id: id })
+}
+
+function add(obj) {
+  return db('schemes').insert(obj, 'id').then(ids => {
+    const [ id ] = ids;
+
+    return findById(id);
+  })
+}
